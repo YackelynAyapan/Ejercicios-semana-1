23669032087ddbc1b85e9a003be86adbde6da788
YackelynAyapan <yackelynestefany@yahoo.com>
2016-07-23 09:52:27 -0600

    tercer

---
+++ ejercicio 3.cs
@@ -0,0 +1,18 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int n1, n2, r;
+            Console.WriteLine("Ingrese el primer número: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+            r = n1 / n2;
+            Console.WriteLine("El resultados es: " + r);
+            Console.ReadLine();
+		
+		
+	}
+}
