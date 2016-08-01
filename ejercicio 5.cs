665adc898727586765d7df805df350337340e4d3
YackelynAyapan <yackelynestefany@yahoo.com>
2016-07-31 21:51:01 -0600

    quinto

---
+++ ejercicio 5.cs
@@ -0,0 +1,22 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+		
+	{int a,b;
+	 Console.WriteLine("Ingrese el primer numero:");
+	 a=int.Parse(Console.ReadLine());
+	 
+	 Console.WriteLine("Ingrese el segundo numero:");
+	 b=int.Parse(Console.ReadLine());
+	 
+	Console.WriteLine("primer numero:"+b);
+	 
+	Console.WriteLine("segundo numero:"+a);
+	 
+	 
+		
+	}
+	
+}
