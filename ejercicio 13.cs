3034a9de9f804e254ee7479dc1208aca56f0a265
YackelynAyapan <yackelynestefany@yahoo.com>
2016-07-31 22:06:08 -0600

    treceavo

---
+++ ejercicio 13.cs
@@ -0,0 +1,22 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{	
+	 int x;
+	 Console.WriteLine("Ingrese un numero");
+	 x=int.Parse(Console.ReadLine());
+	;
+
+	Console.WriteLine(x+""+ x + "" + x );
+
+	Console.WriteLine(x+""+ x + "" + x  );
+		
+	Console.WriteLine(x+""+ x + "" + x );
+
+	Console.WriteLine(x+""+ x + "" + x );
+	Console.WriteLine(x+""+ x + "" + x );
+	
+	}
+}
