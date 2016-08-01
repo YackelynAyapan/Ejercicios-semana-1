0aa849784ae8fa379214972b803b64cdc9049d59
YackelynAyapan <yackelynestefany@yahoo.com>
2016-07-31 22:04:13 -0600

    noveno

---
+++ ejercicio 9.cs
@@ -0,0 +1,24 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+		
+	{int a,b,c,d,r;
+	 Console.WriteLine("Ingrese el primer numero:");
+	 a=int.Parse(Console.ReadLine());
+	Console.WriteLine("Ingrese el segundo numero:");
+	 b=int.Parse(Console.ReadLine());
+	 Console.WriteLine("Ingrese el tercer numero:");
+	 c=int.Parse(Console.ReadLine());
+	 Console.WriteLine("Ingrese el cuarto numero:");
+	 d=int.Parse(Console.ReadLine());
+	 
+	 r=(a+b+c+d)/4;
+	 
+	Console.WriteLine("su promedio es: " + r);
+	 
+	 	
+	}
+	
+}
