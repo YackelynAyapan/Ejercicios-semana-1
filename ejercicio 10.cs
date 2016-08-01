d25cbbaa3893faa0a18e5e142440e69449794308
YackelynAyapan <yackelynestefany@yahoo.com>
2016-07-31 22:11:59 -0600

    decimo

---
+++ ejercicio 10.cs
@@ -0,0 +1,19 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int x1, x2, x3;
+            Console.WriteLine("Ingrese el primer número: ");
+            x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            x2 = int.Parse(Console.ReadLine());
+		    Console.WriteLine("Ingrese el tercer número: ");
+            x3 = int.Parse(Console.ReadLine());
+           
+            Console.WriteLine( x1+","+x2+"y"+ x3+"(x+y)=z es "+x3+x3+"y  x+ x * y * z es:");
+            Console.ReadLine();
+		
+	}
+}
