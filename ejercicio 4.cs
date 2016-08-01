3108023c3dc0d9608e202eae1a9c33da9ea7f36f
YackelynAyapan <yackelynestefany@yahoo.com>
2016-07-31 21:48:39 -0600

    cuarto

---
+++ ejercicio 4.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine(-1+3*5);
+		Console.WriteLine(24+5*7/100);
+		Console.WriteLine(15+-4*6/11);
+		Console.WriteLine(2+10/6*1-7%2);
+		
+	}
+}
