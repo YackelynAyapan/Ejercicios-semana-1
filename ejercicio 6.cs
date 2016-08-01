9362f62e3d8d8f26976828bc9f997d805469873d
YackelynAyapan <yackelynestefany@yahoo.com>
2016-07-31 21:51:31 -0600

    sexto

---
+++ ejercicio 6.cs
@@ -0,0 +1,25 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+		
+	{int a,b,c,r;
+	 Console.WriteLine("Ingrese el primer numero:");
+	 a=int.Parse(Console.ReadLine());
+	 
+	 Console.WriteLine("Ingrese el segundo numero:");
+	 b=int.Parse(Console.ReadLine());
+	  
+	 Console.WriteLine("Ingrese el tercer numero:");
+	 c=int.Parse(Console.ReadLine());
+	 
+   	 r=a*b*c;
+	 
+	 Console.WriteLine(a+"x"+b +"x"+c+"=" +r);  
+	 
+	 
+		
+	}
+
+}
